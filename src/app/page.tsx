3ce9f9aaa16284413481e89f6d4956280d7b932e
Bagdat <clonn262@gmail.com>
2024-08-26 19:35:45 +0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import MapComponent from "../components/MapComponent";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold p-1">Mapbox 3D Buildings</h1>
-      <MapComponent />
-    </div>
+    // <div className="flex flex-col items-center justify-center min-h-screen">
+    <MapComponent />
+    // </div>
   );
 }
